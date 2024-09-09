8bbd00a5f2c7e58bdfb9e34ef8450a5f1e12cf1c
guilhermelkx7@gmail.com <guilhermelkx7@gmail.com>
2024-09-09 09:01:19 -0300

    atividade_04.py

---
+++ atividade_04.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+nota1 = float(input("digete a primeira nota: "))
+nota2 = float(input("digete a segunda nota: "))
+nota3 = float(input("digite a terceira nota: "))
+        
+media = (nota1 + nota2 + nota3)/3
+
+print(f" a média arimética das notas é: {media}")
+
+
+        
